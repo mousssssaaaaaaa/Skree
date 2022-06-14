19fc5854223a5a21d6d136ab7fb8bfe8ecd7253d
NienkeTan <ntan@live.nl>
2022-06-14 12:03:01 +0200

    algorithm outside chain

--- code/classes/chain.py
+++ code/classes/chain.py
@@ -4,28 +4,16 @@ class Chain():
     def __init__(self, aminocode):
         self.folds = [(0,0)]
         self.score = 0
-        self.output = ()
         self.aminocode = aminocode
-        self.wrong_option = ()
 
-    def build(self):
-        # check previous coordinates in folds
-        options = self.get_options()
-        while options == []:
-            self.remove_point()
-            options = self.get_options()
-
-        # find next coordinate random
-        next_point = alg_r.algorithm_random(options)
-
-        self.folds.append(next_point)
+    def build(self, point):
+        self.folds.append(point)
 
     def get_options(self):
         """Get all possible options to move to from current point"""
 
         all_options = self.get_neighbours(self.folds[-1])
-
-        options = all_options - all_options.intersection(set(self.folds)) - set([self.wrong_option])
+        options = all_options - all_options.intersection(set(self.folds)) 
 
         return list(options)
 
@@ -38,8 +26,7 @@ class Chain():
 
         return {top, bottom, left, right}
 
-    def remove_point(self):
-        self.wrong_option = self.folds[-1]
+    def remove_last_point(self):
         self.folds.pop(-1)
 
     def get_score(self):
