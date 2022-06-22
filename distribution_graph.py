c6e93506cbc23f98d8696c5953d217fae2e46e2f
Jason Zhen <jason.zhen@student.uva.nl>
2022-06-22 02:41:51 +0200

    cleanup visualisation.py

--- distribution_graph.py
+++ distribution_graph.py
@@ -115,7 +115,9 @@ def distribution():
     plt.ylabel("P")
     plt.xlabel("scores")
     plt.title("Depth first")
+    plt.show()
 
+    # save produced image
     plt.savefig("results/graph.png")
     plt.close()
 
