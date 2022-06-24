6c8c1b8a94e3f0b89eb7d0c9b7611a317cfac4cb
NienkeTan <ntan@live.nl>
2022-06-24 11:38:57 +0200

    add C

--- Main.py
+++ Main.py
@@ -21,7 +21,7 @@ def main():
 
 
     " ---------------------------------- Random ------------------------------------------- "
-    # chain_result = rnd.algorithm_random(chain)
+    chain_result = rnd.algorithm_random(chain)
 
     " --------------------------------- Depth First ---------------------------------------- "
     # depth_first = df.DepthFirst(chain)
@@ -52,4 +52,14 @@ HHPHHHPHPHHHPH
 HPHPPHHPHPPHPHHPPHPH
 PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP
 HHPHPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHH
+"""
+
+"""
+PPCHHPPCHPPPPCHHHHCHHPPHHPPPPHHPPHPP
+
+CPPCHPPCHPPCPPHHHHHHCCPCHPPCPCHPPHPC
+
+HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH
+
+HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH
 """
